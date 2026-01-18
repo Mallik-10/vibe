60bd2fdcfb0e4c61fe0fae1578a620577f377787
eF-Zay <farjulmallik10@gmail.com>
2026-01-18 19:19:57 +0000

    added trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@/components/ui/button"
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-const Page = () => {
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "Hello this is void"}))
   return(
-    <div className="font-bold">
-      <Button>
-        Click me
-      </Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading the response...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 export default Page
---
+++ src/trpc/server.tsx
@@ -0,0 +1,24 @@
+import 'server-only'; // <-- ensure this file cannot be imported from the client
+import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
+import { cache } from 'react';
+import { createTRPCContext } from './init';
+import { makeQueryClient } from './query-client';
+import { appRouter } from './routers/_app';
+//import { createTRPCClient, httpLink } from '@trpc/client';
+
+export const getQueryClient = cache(makeQueryClient);
+
+export const trpc = createTRPCOptionsProxy({
+  ctx: createTRPCContext,
+  router: appRouter,
+  queryClient: getQueryClient,
+});
+// If your router is on a separate server, pass a client:
+// createTRPCOptionsProxy({
+//   client: createTRPCClient({
+//     links: [httpLink({ url: '...' })],
+//   }),
+//   queryClient: getQueryClient,
+// });
+
+export const caller = appRouter.createCaller(createTRPCContext);
